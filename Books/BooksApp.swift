83877155c559ecf6c8b56d23d11374bb0416e8e6
Manisankar <msshankar.ch@gmail.com>
2025-01-31 12:09:20 +0530

    Initial Commit

---
+++ Books/BooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksApp.swift
+//  Books
+//
+//  Created by Softsuave-iOS dev on 31/01/25.
+//
+
+import SwiftUI
+
+@main
+struct BooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
